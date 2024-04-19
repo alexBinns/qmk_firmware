c86d1ef4def0acaf54445a14783bad65d9269609
Alex Binns <alex.binns@centrapay.com>
2024-04-20 11:32:36 +1200

    Add first 2 keymaps

---
+++ keyboards/crkbd/keymaps/mine/rules.mk
@@ -0,0 +1,23 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+# markstos: enable media keys
+EXTRAKEY_ENABLE = yes
+
+# markstos: smaller file size, little down-side
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+# markstos: for combos!
+COMBO_ENABLE = yes
+
+# markstos
+# Backlighting is not RGB underglow or RGB matrix
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+
+# This is for RGB *underglow*
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.mdy
+RGBLIGHT_ENABLE = no
+
---
+++ keyboards/crkbd/keymaps/rev2/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+AUTO_SHIFT_ENABLE = no
+COMBO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
