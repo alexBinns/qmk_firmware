c55285c11f75256729511438f0668b64baaec88e
alexBinns <alex.binns@centrapay.com>
2026-03-17 07:43:48 +1300

    Update config

---
+++ keyboards/crkbd/keymaps/miryoku/keymap.c
@@ -0,0 +1,83 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+const uint16_t PROGMEM JK_ESC[] = {KC_J, KC_K, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(JK_ESC, KC_ESC)
+};
+
+#define _BASE 0
+#define _MEDIA 3
+#define _SYM 5
+#define _NAV 1
+#define _FUN 6
+#define _NUM 4
+#define _MOUSE 2
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_split_3x6_3(
+     KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ESC, \
+     KC_NO, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT), KC_NO, \
+     KC_NO, KC_Z, ALGR_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, ALGR_T(KC_DOT), KC_SLSH, KC_NO, \
+     LT(_MEDIA,KC_ESC), LT(_NAV,KC_SPC), LT(_MOUSE,KC_TAB), LT(_SYM,KC_ENT), LT(_NUM,KC_BSPC), LT(_FUN,KC_DEL)
+  ),
+
+  [_NAV] = LAYOUT_split_3x6_3(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, SCMD(KC_Z), LCMD(KC_V), LCMD(KC_C), LCMD(KC_X), LCMD(KC_Z), KC_NO, \
+    KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, CW_TOGG, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, \
+    KC_NO, _______, KC_NO, KC_ENT, KC_BSPC, KC_DEL
+  ),
+
+  [_MOUSE] = LAYOUT_split_3x6_3(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, SCMD(KC_Z), LCMD(KC_V), LCMD(KC_C), LCMD(KC_X), LCMD(KC_Z), KC_NO, \
+    KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_NO, \
+    KC_NO, KC_NO, _______, KC_BTN1, KC_BTN2, KC_BTN3
+  ),
+
+  [_MEDIA] = LAYOUT_split_3x6_3(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    _______, KC_NO, KC_NO, KC_MSTP, KC_MPLY, KC_MUTE
+  ),
+
+  [_NUM] = LAYOUT_split_3x6_3(
+    KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, \
+    KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_DOT, KC_0, KC_MINS, KC_NO, _______, KC_NO
+  ),
+
+  [_SYM] = LAYOUT_split_3x6_3(
+    KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, \
+    KC_NO, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_LPRN, KC_RPRN, KC_UNDS, _______, KC_NO, KC_NO
+  ),
+
+  [_FUN] = LAYOUT_split_3x6_3(
+    KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL, KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, \
+    KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_APP, KC_SPC, KC_TAB, KC_NO, KC_NO, _______
+  )
+};
--- keyboards/crkbd/keymaps/rev2/rules.mk
+++ keyboards/crkbd/keymaps/rev2/rules.mk
@@ -1,7 +1,7 @@
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+# OLED_ENABLE     = yes
+# OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 AUTO_SHIFT_ENABLE = no
 COMBO_ENABLE = yes
